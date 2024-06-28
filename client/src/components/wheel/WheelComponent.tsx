622e14b05a9d8772d84e3f4b6108e28373855e0b
katredford <katharine.redford@gmail.com>
2024-06-27 21:44:43 -0500

    progress on wheel animation

--- client/src/components/wheel/WheelComponent.tsx
+++ client/src/components/wheel/WheelComponent.tsx
@@ -2,7 +2,7 @@
 import { FC, useEffect, useRef, useState } from 'react';
 // import { useParams } from 'react-router-dom';
 import { useWheel } from '../context/useWheel';
-import './wheel.css';
+// import './wheel.css';
 
 
 
@@ -12,9 +12,7 @@ import './wheel.css';
 const WheelComponent: FC = () => {
     // const { id } = useParams<{ id: string }>();
     const { oneWheel, loading, spinAnimationTriggered } = useWheel();
-    const radius = 200;
-    const strokeColor = 'black';
-    const strokeWidth = 4;
+
     const [isSpinning, setIsSpinning] = useState(false);
     const [wheelPos, setWheelPos] = useState(0);
     const [speed, setSpeed] = useState(0);
@@ -26,17 +24,41 @@ const WheelComponent: FC = () => {
     const spinTime = 200000;
     const requestRef = useRef<number>(0);
 
-    const generateSlicePath = (index: number, total: number): string => {
-        const angle = (2 * Math.PI) / total;
-        const startAngle = index * angle;
-        const endAngle = startAngle + angle;
-        const startX = radius + radius * Math.cos(startAngle);
-        const startY = radius + radius * Math.sin(startAngle);
-        const endX = radius + radius * Math.cos(endAngle);
-        const endY = radius + radius * Math.sin(endAngle);
-        const largeArcFlag = angle > Math.PI ? 1 : 0;
-        return `M ${radius},${radius} L ${startX},${startY} A ${radius},${radius} 0 ${largeArcFlag} 1 ${endX},${endY} Z`;
-    };
+     //radious of the wheel
+     const radius: number = 200;
+     //color of the lines
+     const strokeColor: string = 'black';
+     //thickness of the lines
+     const strokeWidth: number = 4;
+ 
+     // function to generate the SVG path for a slice
+     const generateSlicePath = (index: number, total: number): string => {
+         //calculates the angle of each slice by divideing the circle
+         // 2π by the number of items
+         const angle = (2 * Math.PI) / total;
+ 
+         //calculate the start and end angles of the slice
+         const startAngle = index * angle;
+         const endAngle = startAngle + angle;
+ 
+         // calculate the start and end points of the slice
+         const startX = radius + radius * Math.cos(startAngle);
+         const startY = radius + radius * Math.sin(startAngle);
+         const endX = radius + radius * Math.cos(endAngle);
+         const endY = radius + radius * Math.sin(endAngle);
+ 
+         //determines whether the arc should be greater than 180 degrees.
+         //it's set to 1 if the angle is greater than π, otherwise, it's 0.
+         const largeArcFlag = angle > Math.PI ? 1 : 0;
+ 
+ 
+         //svg paths:
+         //M =move to takes 2 points
+         //L =lineTo : creating a line
+         //A =eliptical arch: for the circle edge
+         //Z =close path
+         return `M ${radius},${radius} L ${startX},${startY} A ${radius},${radius} 0 ${largeArcFlag} 1 ${endX},${endY} Z`;
+     };
 
     const calculateTextPosition = (index: number, total: number): { x: number, y: number, angle: number } => {
         const angle = (2 * Math.PI) / total;
@@ -73,20 +95,27 @@ const WheelComponent: FC = () => {
         return x * (endPos - startPos) + startPos;
     };
 
-    const animate = (time: number) => {
+    const animate = () => {
+        //performance now method more precise than date.now()
+        //captures the current time in millisecons
         const currentTime = performance.now();
         if (isSpinning) {
+            //uses wheelPosFunction, to calculate new position of the wheel
             const newWheelPos = wheelPosFunction(currentTime, startTime, endTime, wheelPos, endPos);
             setWheelPos(newWheelPos);
             if (currentTime >= endTime) {
                 setIsSpinning(false);
                 setSpeed(0);
             }
-        } else {
+        } 
+        else {
             setSpeed(speed * 0.95);
             setWheelPos(wheelPos + speed);
         }
 
+        //requests the next animation frame using requestAnimationFrame(animate), 
+        //which recursively calls the animate function. This creates a continuous loop 
+        //for smooth animation.
         requestRef.current = requestAnimationFrame(animate);
     };
 
@@ -115,9 +144,27 @@ const WheelComponent: FC = () => {
     },[spinAnimationTriggered])
 
     useEffect(() => {
+
+        //This line sets up an animation frame request. requestAnimationFrame(animate) 
+        //schedules the animate function to be called before the next repaint. The return 
+        //value is stored in requestRef.current so it can be referenced later. requestRef 
+        //is a useRef hook, which is used to persist values between renders without 
+        //causing re-renders
+     
         requestRef.current = requestAnimationFrame(animate);
+
+        //This line defines a cleanup function that cancels the scheduled animation frame
+        // request when the component unmounts or when the useEffect dependencies change. 
+        //This prevents memory leaks and ensures that no animation frames are requested when 
+        //they are not needed.
         return () => cancelAnimationFrame(requestRef.current);
-    }, [isSpinning, wheelPos, speed]);
+
+        //This array contains the dependencies for the useEffect hook. The effect will 
+        //run whenever any of these dependencies (isSpinning, wheelPos, speed, spinAnimationTriggered)
+        // change. It also ensures that the cleanup function is executed when 
+        //any of these dependencies change.
+
+    }, [isSpinning, wheelPos, speed, spinAnimationTriggered]);
 
     if (loading) {
         return <div>Loading...</div>;
@@ -132,7 +179,7 @@ const WheelComponent: FC = () => {
             <h1>{oneWheel?.title}</h1>
             <svg width={2 * radius} height={2 * radius} style={{ overflow: 'visible' }}
                 // onClick={handleSpinClick} 
-                className={spinAnimationTriggered ? 'spin-animation' : ''}
+                // className={spinAnimationTriggered ? 'spin-animation' : ''}
                 >
 
                 {/* Wheel slices group with rotation */}
@@ -178,200 +225,6 @@ const WheelComponent: FC = () => {
 
 export default WheelComponent;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //FC is functional component used to define in typescript
-// import { FC, useEffect, useRef, useState } from 'react';
-// // import { useParams } from 'react-router-dom';
-// import { useWheel } from '../context/useWheel';
-// import './wheel.css';
-
-
-
-
-
-// //functional component that recievs title and items props from valuesControl
-// const WheelComponent: FC = () => {
-//     // const { id } = useParams<{ id: string }>();
-//     const { oneWheel, loading} = useWheel();
-//     const requestRef = useRef<number>(0);
-
-//     const animate = () => {
-//         requestRef.current = requestAnimationFrame(animate);
-//         // Your animation logic here if needed
-//     };
-
-//     useEffect(() => {
-//         requestRef.current = requestAnimationFrame(animate);
-//         return () => cancelAnimationFrame(requestRef.current);
-//     }, []);
-
-//     // const handleSpinClick = () => {
-//     //     triggerSpinAnimation(); // Call context function to start spin animation
-//     // };
-
-//     // console.log("Wheel Component: spinAnimationTriggered", spinAnimationTriggered)
-
-
-//     //radious of the wheel
-//     const radius: number = 200;
-//     //color of the lines
-//     const strokeColor: string = 'black';
-//     //thickness of the lines
-//     const strokeWidth: number = 4;
-
-//     // function to generate the SVG path for a slice
-//     const generateSlicePath = (index: number, total: number): string => {
-//         //calculates the angle of each slice by divideing the circle
-//         // 2π by the number of items
-//         const angle = (2 * Math.PI) / total;
-
-//         //calculate the start and end angles of the slice
-//         const startAngle = index * angle;
-//         const endAngle = startAngle + angle;
-
-//         // calculate the start and end points of the slice
-//         const startX = radius + radius * Math.cos(startAngle);
-//         const startY = radius + radius * Math.sin(startAngle);
-//         const endX = radius + radius * Math.cos(endAngle);
-//         const endY = radius + radius * Math.sin(endAngle);
-
-//         //determines whether the arc should be greater than 180 degrees.
-//         //it's set to 1 if the angle is greater than π, otherwise, it's 0.
-//         const largeArcFlag = angle > Math.PI ? 1 : 0;
-
-
-//         //svg paths:
-//         //M =move to takes 2 points
-//         //L =lineTo : creating a line
-//         //A =eliptical arch: for the circle edge
-//         //Z =close path
-//         return `M ${radius},${radius} L ${startX},${startY} A ${radius},${radius} 0 ${largeArcFlag} 1 ${endX},${endY} Z`;
-//     };
-
-//     // Function to calculate the position of the text within each slice
-//     const calculateTextPosition = (index: number, total: number): { x: number, y: number, angle: number } => {
-//         const angle = (2 * Math.PI) / total;
-//         const midAngle = index * angle + angle / 2;
-//         const textRadius = radius * 0.8;
-//         const x = radius + textRadius * Math.cos(midAngle);
-//         const y = radius + textRadius * Math.sin(midAngle);
-//         const rotation = (midAngle * 180) / Math.PI - 90;
-//         return { x, y, angle: rotation };
-//     };
-
-//     if (loading) {
-//         return <div>Loading...</div>;
-//     }
-
-//     if (!oneWheel) {
-//         return <div>Wheel not found</div>;
-//     }
-
-//     // function to split text into chunks by preserving whole words
-//     const splitByWords = (text: string, maxLength: number): string[] => {
-//         const chunks: string[] = [];
-//         let currentChunk = '';
-
-//         text.split(' ').forEach(word => {
-//             if (currentChunk.length + word.length <= maxLength) {
-//                 currentChunk += (currentChunk === '' ? '' : ' ') + word;
-//             } else {
-//                 chunks.push(currentChunk);
-//                 currentChunk = word;
-//             }
-//         });
-
-//         if (currentChunk !== '') {
-//             chunks.push(currentChunk);
-//         }
-
-//         return chunks;
-//     };
-
-
-
-//     // const animate = (time: number) => {
-//     //     const currentTime = performance.now();
-//     //     if (spin) {
-//     //         const newWheelPos = wheelPosFunction(currentTime, startTime, endTime, wheelPos, endPos);
-//     //         setWheelPos(newWheelPos);
-//     //         if (currentTime >= endTime) {
-//     //             setIsSpinning(false);
-//     //             setSpeed(0);
-//     //         }
-//     //     } else {
-//     //         setSpeed(speed * 0.95);
-//     //         setWheelPos(wheelPos + speed);
-//     //     }
-
-//     //     requestRef.current = requestAnimationFrame(animate);
-//     // };
-
-
-//     return (
-//         <>
-//             <h1>{oneWheel?.title}</h1>
-//             {/* creates an SVG element with a width and height equal to 
-//             twice the radius (to accommodate the full circle).    */}
-//             <svg width={2 * radius} height={2 * radius} 
-//             style={{ overflow: 'visible' }}
-//             //     className={spinAnimationTriggered ? 'spin-animation' : ''}
-                
-//                 >
-
-//                 {oneWheel.Values.map((value, i) => {
-//                     const { x, y, angle } = calculateTextPosition(i, oneWheel.Values.length);
-//                     // Split value.value into chunks of 30 characters
-//                     const chunks = splitByWords(value.value, 25)
-//                     return (
-//                         <g key={i}>
-//                             <path
-//                                 d={generateSlicePath(i, oneWheel.Values.length)}
-//                                 fill="none"
-//                                 stroke={strokeColor}
-//                                 strokeWidth={strokeWidth}
-//                             />
-//                             {chunks.map((chunk, j) => (
-//                                 <text
-//                                     key={`${i}-${j}`}
-//                                     x={x}
-//                                     y={y + j * 12} // adjust y position of each line, brings it closer to outer edge
-//                                     transform={`rotate(${angle + 180}, ${x}, ${y})`}
-//                                     textAnchor="middle"
-//                                     alignmentBaseline="middle"
-//                                     style={{ fontSize: '12px' }}
-//                                 >
-//                                     {chunk}
-//                                 </text>
-//                             ))}
-//                         </g>
-//                     );
-//                 })}
-//             </svg>
-//         </>
-//     );
-// };
-
-
-// export default WheelComponent;
 
 
 
